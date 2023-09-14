59bd9f7fbf3b14dc1b27a36dbff10a60e6ce7bd3
SandraS2611 <sssandra1980@gmail.com>
2023-09-14 00:18:22 -0300

    elimine const que iba probando y ya no me sirven

--- index.js
+++ index.js
@@ -17,43 +17,11 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs"); //motor de plantillas ejs
 
-// const places = [
-//   {
-//     location: "Santiago del Estero",
-//     description: "kjdhjhdjfhg",
-//     date: "26/11/1980",
-//   },
-//   {
-//     location: "La Banda",
-//     description: "fjdfljhjgihdfgjl",
-//     date: "07/03/2016",
-//   },
-//   {
-//     location: "Termas del Río Hondo",
-//     description: "jsdhfjhsdjhdf",
-//     date: "14/08/2020",
-//   },
-//   {
-//     location: "Tucumán",
-//     description: "ejashfsdhjfhd",
-//     date: "22/09/1983",
-//   },
-//   {
-//     location: "Santa Fe",
-//     description: "ejashfsdhjfhd",
-//     date: "22/09/1910",
-//   },
-// ];
-
 app.use("/places", require("./routes/places.routes"));
 
 app.get("/", (req, res) => {
   res.render("index", { título: "Proyecto Final de Sandra S.", places });
 });
-
-// app.get("/imagenes", (req, res) => {
-//   res.render("imagenes", { título: "Lugares", places });
-// });
 
 app.listen(3000, () => {
   sequelize.sync({ force: false });
