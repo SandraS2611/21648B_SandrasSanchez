edb3a959656199a545ed6e43361624f0638c7a39
SandraS2611 <sssandra1980@gmail.com>
2023-09-14 00:16:33 -0300

    elimine la carpeta crs

--- csr/main.js
+++
@@ -1,7 +0,0 @@
-const container = document.getElementById("container")
-
-fetch("http://localhost:2023/places")
-     .then(res => res.json())
-     .then(data => {
-        container.innerHTML = data.map(e => `<p>${e.location}<p>`).join("")
-     })
