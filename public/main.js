39e19f3336c3acf6f013af7eda9b91fb88b4177c
SandraS2611 <sssandra1980@gmail.com>
2023-09-21 11:37:12 -0300

    alerts correctos

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const b_back = document.getElementById("b_back")
 const b_up = document.getElementById("b_up")
+const b_delete = document.getElementById("b_delete")
 
 b_back.addEventListener("click", () => {
     alert("Volviendo al inicio"); 
@@ -7,4 +8,8 @@ b_back.addEventListener("click", () => {
 
 b_up.addEventListener("click", () => {
     alert("Post creado"); 
+})
+
+b_delete.addEventListener("click", () => {
+    alert("Post eliminado"); 
 })
