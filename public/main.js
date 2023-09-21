61ca8895148ed2248563c69c0210ee64fbb923c5
SandraS2611 <sssandra1980@gmail.com>
2023-09-21 11:15:03 -0300

    alerts en botones

---
+++ public/main.js
@@ -0,0 +1,10 @@
+const b_back = document.getElementById("b_back")
+const b_up = document.getElementById("b_up")
+
+b_back.addEventListener("click", () => {
+    alert("Volviendo al inicio"); 
+})
+
+b_up.addEventListener("click", () => {
+    alert("Post creado"); 
+})
